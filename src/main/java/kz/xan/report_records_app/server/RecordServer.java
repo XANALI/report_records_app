a3657f89348d7c79302033a92eeb8b93affdc8c1
XANALI <53555116+XANALI@users.noreply.github.com>
2020-08-03 22:24:27 +0600

    Added RecordServer. closes #1

---
+++ src/main/java/kz/xan/report_records_app/server/RecordServer.java
@@ -0,0 +1,34 @@
+package kz.xan.report_records_app.server;
+
+import kz.xan.report_records_app.bootstrap.DataLoader;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import static kz.xan.report_records_app.client.config.ConnectionConfig.PORT_NUMBER;
+
+public class RecordServer {
+
+    public static void main(String[] args) {
+        try{
+            // initializing of a server with defined port
+            ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
+            DataLoader dataLoader = new DataLoader();
+
+            while (true) {
+                //listening for a client socket request
+                Socket socket = serverSocket.accept();
+                System.out.println("Client connected: " + socket.getInetAddress().getHostAddress());
+
+                // Making a new ClientHandler for each client
+                ClientHandler clientHandler = new ClientHandler(socket, dataLoader);
+
+                // Starting of relations client and server
+                clientHandler.start();
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
