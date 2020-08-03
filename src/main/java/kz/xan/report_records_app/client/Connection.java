35dc1fed584d8ea7ccc06b1a2c75091ef1b48c0e
XANALI <53555116+XANALI@users.noreply.github.com>
2020-08-03 22:26:18 +0600

    Added Client's Connection class. closes #4

---
+++ src/main/java/kz/xan/report_records_app/client/Connection.java
@@ -0,0 +1,63 @@
+package kz.xan.report_records_app.client;
+
+
+import kz.xan.report_records_app.server.Reply;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class Connection {
+    private Socket socket;
+    private ObjectInputStream ois;
+    private ObjectOutputStream oos;
+
+    public Connection() {
+    }
+
+    public Connection(Socket socket) throws IOException {
+        this.socket = socket;
+        oos = new ObjectOutputStream(socket.getOutputStream());
+        ois = new ObjectInputStream(socket.getInputStream());
+    }
+
+    public void sendRequest(Request request){
+        try {
+            oos.writeObject(request);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Reply getReply(){
+        try {
+            return (Reply) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public void closeInputStream() throws IOException {
+        if(ois != null) {
+            ois.close();
+            ois = null;
+        }
+    }
+
+    public void closeOutputStream() throws IOException {
+        if(oos != null){
+            oos.close();
+            oos = null;
+        }
+    }
+
+    public void closeSocket() throws IOException {
+        if(socket != null){
+            socket.close();
+            socket = null;
+        }
+    }
+}
