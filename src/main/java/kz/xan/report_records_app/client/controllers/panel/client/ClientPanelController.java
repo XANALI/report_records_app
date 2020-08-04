91cc25d277baa64b8dc6b49bef987f57cac8782d
XANALI <53555116+XANALI@users.noreply.github.com>
2020-08-04 16:41:55 +0600

    Added panel UserController. closes #10

---
+++ src/main/java/kz/xan/report_records_app/client/controllers/panel/client/ClientPanelController.java
@@ -0,0 +1,35 @@
+package kz.xan.report_records_app.client.controllers.panel.client;
+
+import kz.xan.report_records_app.client.Connection;
+import kz.xan.report_records_app.client.controllers.panel.PanelController;
+import kz.xan.report_records_app.domain.User;
+
+import java.util.Scanner;
+
+public class ClientPanelController extends PanelController {
+
+    public ClientPanelController(Connection connection, Scanner scanner, User client) {
+        super(connection, scanner, client);
+    }
+
+    @Override
+    public void showServices() {
+        while(true){
+            System.out.println("User Panel");
+            System.out.println("Choose one service:");
+            System.out.println("1. Profile");
+            System.out.println("2. Records");
+            System.out.println("3. Reports");
+            System.out.println("0. Logout");
+
+            int choice = scanner.nextInt();
+
+            if(choice == 0){
+                break;
+            }else if(choice == 1){
+                showProfile();
+            }
+        }
+    }
+
+}
